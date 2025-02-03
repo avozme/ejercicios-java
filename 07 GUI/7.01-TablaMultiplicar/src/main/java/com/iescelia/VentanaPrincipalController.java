62e05853624a944a10afcef338f2a0e1de0d7264
avozme <avozme@yahoo.com>
2025-02-03 18:50:49 +0100

    Añado ejercicios resueltos con Maven del tema 7 - GUI

---
+++ 07 GUI/7.01-TablaMultiplicar/src/main/java/com/iescelia/VentanaPrincipalController.java
@@ -0,0 +1,29 @@
+package com.iescelia;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class VentanaPrincipalController {
+    @FXML
+    TextField numero;
+
+    @FXML
+    TextArea salida;
+
+    @FXML
+    private void mostrarTabla(Event e) {
+        String tabla = "";
+        int n = Integer.parseInt(numero.getText());
+        for (int i = 1; i <= 10; i++) {
+            tabla += n + " x " + i + " = " + (n*i) + "\n";
+        }
+        salida.setText(tabla);
+    }
+
+    @FXML
+    private void borrarTabla(Event e) {
+        salida.setText("");
+    }    
+}
---
+++ 07 GUI/7.02-LoteriaPrimitiva/src/main/java/com/iescelia/VentanaPrincipalController.java
@@ -0,0 +1,25 @@
+package com.iescelia;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class VentanaPrincipalController {
+    @FXML
+    TextField salida;
+
+
+    @FXML
+    private void generarCombinacion(Event e) {
+        String str = "";
+        for (int i = 1; i <= 6; i++) {
+            str += (int)(Math.random() * 49) + 1 + "  ";
+        }
+        salida.setText(str);
+    }
+
+    @FXML
+    private void borrarTabla(Event e) {
+        salida.setText("");
+    }    
+}
